cdd992e2eb40b882321e89215259fe7489c5db16
JoyceYang2018 <384381523@qq.com>
2018-06-12 23:53:08 -0700

    3a

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 #coding:utf-8
 
 #初始化一个Flask对象
-from flask import Flask,request
+from flask import Flask,request,render_template
 from flask_script import Manager
 
 app = Flask(__name__)
@@ -12,12 +12,11 @@ manager = Manager(app)#把程序实例作为参数传给构造函数，初始化
 @app.route('/')
 def index():
     #request在一个线程中全局可访问，这就是flask上下文
-    user_agent= request.headers.get('User-Agent')
-    return '<p>Your Browser is %s</p>'%user_agent
+    return render_template('index.html')
 
 @app.route('/user/<name>')#动态部分，默认是字符串
 def user(name):
-    return '<h1>Hello, %s!</h1>'%name
+    return render_template('user.html',name=name)
 
 if __name__ == '__main__':
     manager.run()#轮询，启用调试模式可以激活调试器和重载程序
