23be36e4f5f1be222a50bbc71a26efff1902bf2a
JoyceYang2018 <384381523@qq.com>
2018-06-13 00:19:06 -0700

    3b

--- hello.py
+++ hello.py
@@ -3,10 +3,13 @@
 #初始化一个Flask对象
 from flask import Flask,request,render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 manager = Manager(app)#把程序实例作为参数传给构造函数，初始化主类的实例，创建的对象可以在各个扩展中使用
+bootstrap = Bootstrap(app)
+
 
 #路由和视图函数
 @app.route('/')
