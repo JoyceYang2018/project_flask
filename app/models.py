552860d42fb80e372c26b10a8e885beed99e01a3
JoyceYang2018 <384381523@qq.com>
2018-06-14 00:33:12 -0700

    8a

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
+#coding:utf-8
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 class Role(db.Model):
@@ -19,3 +21,19 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+    password_hash = db.Column(db.String(128))
+
+
+    #不可读
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+    
+    #只可写
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
