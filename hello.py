3391ccab777f6070694d6fb1b67088c1ed2538c6
JoyceYang2018 <384381523@qq.com>
2018-06-13 22:50:00 -0700

    6b

--- hello.py
+++ hello.py
@@ -12,6 +12,7 @@ from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate,MigrateCommand
 from flask_mail import Mail,Message
+from threading import Thread
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -27,7 +28,7 @@ app.config['MAIL_USE_TLS']=True
 app.config['MAIL_USERNAME']=os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD']=os.environ.get('MAIL_PASSWORD')
 app.config['FLASKY_MAIL_SUBJECT_PREFIX']='[Flasky]'
-app.config['FLASKY_MAIL_SENDER']='Flasky Admin <yazhiyang93@gmail.com>'
+app.config['FLASKY_MAIL_SENDER']=os.environ.get('MAIL_USERNAME')
 app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 
 
@@ -48,12 +49,23 @@ manager.add_command('db',MigrateCommand)
 
 
 
+
+#异步发送电子邮件
+def send_async_email(app,msg):
+    with app.app_context():
+        mail.send(msg)
+
 #发送邮件
 def send_email(to,subject,template,**kwargs):
     msg=Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX']+subject,sender=app.config['FLASKY_MAIL_SENDER'],recipients=[to])
     msg.body = render_template(template + '.txt',**kwargs)
     msg.html = render_template(template +'.html',**kwargs)
-    mail.send(msg)
+    thr = Thread(target=send_async_email,args=[app,msg])
+    thr.start()
+    return thr
+
+
+
 
 
 
