2d7bd930f47401f3dfd9b881b13253951ca6efe4
JoyceYang2018 <384381523@qq.com>
2018-06-16 04:16:32 -0700

    11d

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-from flask import render_template,session,redirect,url_for,abort,flash
+from flask import render_template,request,session,redirect,url_for,abort,flash
 
 from . import main
 from .forms import NameForm,EditProfileForm,EditProfileAdminForm,PostForm
 from .. import db
-from ..models import User,Role,Permission,Post
+from ..models import User,Role,Permission,Post,current_app
 from flask_login import login_required,current_user
 from ..decorators import admin_required
 
@@ -17,8 +17,13 @@ def index():
                     author=current_user._get_current_object())
         db.session.add(post)
         return redirect(url_for('.index'))
-    posts=Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html',form=form,posts=posts)
+    page = request.args.get('page',1,type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page,per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False
+    )
+    posts = pagination.items
+    return render_template('index.html',form=form,posts=posts,pagination=pagination)
 
 
 
--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Config:
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = os.environ.get('MAIL_USERNAME')
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_POSTS_PER_PAGE = os.environ.get('FLASKY_POSTS_PER_PAGE')
 
     @staticmethod
     def init_app(app):
@@ -24,6 +25,7 @@ class DevelopmentConfig(Config):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
 
 
 
