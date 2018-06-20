877653ac436d5a341299daa25d2f23ce25973f04
JoyceYang2018 <384381523@qq.com>
2018-06-19 22:13:11 -0700

    12b

--- app/models.py
+++ app/models.py
@@ -86,7 +86,7 @@ class User(UserMixin,db.Model):
                                backref=db.backref('follower',lazy='joined'),
                                lazy = 'dynamic',
                                cascade='all,delete-orphan')
-    follower = db.relationship('Follow',
+    followers = db.relationship('Follow',
                                foreign_keys=[Follow.followed_id],
                                backref=db.backref('followed',lazy='joined'),
                                lazy='dynamic',
