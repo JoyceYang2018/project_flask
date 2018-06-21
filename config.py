60f23984423e9f3d67a0bc25d2adfe1326e0442b
JoyceYang2018 <384381523@qq.com>
2018-06-21 05:37:14 -0700

    17d

--- config.py
+++ config.py
@@ -95,6 +95,20 @@ class HerokuConfig(ProductionConfig):
 
 
 
+class UnixConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls,app):
+        ProductionConfig.init_app(app)
+
+        #写入系统日志
+        import logging
+        from logging.handlers import SysLogHandler
+        syslog_handler = SysLogHandler()
+        syslog_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(syslog_handler)
+
+
+
 
 config = {
     'development':DevelopmentConfig,
--- manage.py
+++ manage.py
@@ -14,6 +14,14 @@ if os.environ.get('FLASKY_COVERAGE'):
     COV.start()
 
 
+if os.path.exists('.env'):
+    print('Importing environment from .env...')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var)==2:
+            os.environ[var[0]] = var[1]
+
+
 
 
 
